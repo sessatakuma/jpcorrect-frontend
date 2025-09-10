c09a56d8e7590d2feebe03008bffa86d876ac0b6
陳曦 <eriksonchen@gmail.com>
2025-09-10 15:03:54 +0800

    Layout for display and transcript

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -5,7 +5,15 @@ import 'components/Display.css';
 export default function Display() {
     return (
         <section className='display'>
-            display
+            <iframe 
+                width="560" 
+                height="315" 
+                src="https://www.youtube.com/embed/3DrYQMK4hJE?si=rJKkERmEbyvmvH1N" 
+                title="YouTube video player" 
+                frameborder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                referrerpolicy="strict-origin-when-cross-origin" 
+                allowfullscreen/>
         </section>
     );
 }
--- src/components/Transcript.jsx
+++ src/components/Transcript.jsx
@@ -1,11 +1,41 @@
 import React from "react";
-
 import 'components/Transcript.css';
 
 export default function Transcript() {
+    const rawText = `
+空っぽのサイレン
+乾いた施錠音(せじょうおと)
+ガラガラスケーター 
+賑わえ三点リーダ
+どんな言葉にも ちゃんと 
+綻びがあった
+もう 何も言わずに
+遊ぼう(遊ぼう)
+
+どうでもいい朝に 慣れ過ぎて 傘は持ってない 借りてた未来 返しきれず かわしきれずからがら まちゆく余所者
+
+ねえ ねえ 独りだ 肩触れたのは メイビー、レイニー 君はとうにいないんだね かなしくはないよ 嬉しくもないよ メイビー、レイニー よく晴れた鈍色に 減点式の舗装路を染める レイニー、レイニー こんな間違えたんだね 塗りつぶしてゆくパノラマ レイニー、レイニー 君は何点だったの
+
+バイバイ バイバイ 
+よく晴れた鈍色のしょうご
+    `;
+
+    // split by line breaks and trim
+    const list = rawText
+        .split('\n')
+        .map(line => line.trim())
+        .filter(line => line.length > 0) // remove empty lines
+        .map(line => ({ name: 'Sui', text: line }));
+
     return (
         <section className='transcript'>
-            transcript
+            <div className="header">文字起こし</div>
+            {list.map((t, i) => (
+                <div className='caption' key={i}>
+                    <figure className="icon"></figure>
+                    <p class='text'>{t.text}</p>
+                </div>
+            ))}
         </section>
-    );
+    )
 }
