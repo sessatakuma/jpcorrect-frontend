42f51aede586045b3019fdac824e079962e8adb9
陳曦 <eriksonchen@gmail.com>
2025-09-11 20:57:21 +0800

    Finish timestamp layout

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import Nav from 'components/Nav';
 import Display from 'components/Display';
 import Transcript from 'components/Transcript';
-import TimeStamps from 'components/TimeStamps';
 import Hint from 'components/Hint';
 import Footer from 'components/Footer';
 
@@ -15,7 +14,6 @@ export default function Main(props) {
         <main>
             <Display/>
             <Transcript/>
-            <TimeStamps/>
             <Hint/>
         </main>
         <Footer/>
