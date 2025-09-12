6da3dbe963adc70b7d36315be572cdde7f722c19
陳曦 <eriksonchen@gmail.com>
2025-09-12 14:37:19 +0800

    refactor: VedioInfo

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,6 +1,7 @@
 import React, { useRef } from "react";
 import YouTube from "react-youtube";
 import 'components/Display.css';
+import VideoInfo from "components/VideoInfo";
 
 export default function Display() {
     const playerRef = useRef(null);
@@ -52,32 +53,7 @@ export default function Display() {
                     </button>
                 </div>
             </div>
-            <div className="info">
-                <div className="video-info">
-                    <h2 className="title">テーマ</h2>
-                    ．
-                    <span className="event-type">検討会</span>
-                    ．
-                    <span className="date">2024/01/01</span>
-                </div>
-                <div className="participant-info">
-                    {[1, 2].map((_, index) => (
-                        <div className="group" key={index}>
-                            <h3 className="title">グループ{index + 1}</h3>
-                            <ul className="participants">
-                                {
-                                    Array.from({ length: 8 }, (_, i) => i + 1).map((_, _index) => (
-                                        <li className="participant" key={index * 8 + _index}>
-                                            <img className="icon" src="https://yt3.ggpht.com/ytc/AIdro_kLDBK5ksSvk5-XJ6S8e0kWfjy7mVl3jyUkgDeMQ7rlCpU=s88-c-k-c0x00ffffff-no-rj" alt="Author Avatar"/>
-                                            <span className="name">すい {index * 8 + _index + 1}</span>
-                                        </li>
-                                    ))
-                                }
-                            </ul>
-                        </div>
-                    ))}
-                </div>
-            </div>
+            <VideoInfo />
         </section>
   );
 }
