36548113e54e13a230162b7b06bf3b6c52f5fea7
陳曦 <94444077+Hsiii@users.noreply.github.com>
2025-10-15 16:18:10 +0800

    style: add prettier as formatter (#15)

--- src/components/Hint.jsx
+++ src/components/Hint.jsx
@@ -1,18 +1,18 @@
-import React from "react";
+import React from 'react';
 
 import 'components/Hint.css';
 
 export default function Hint() {
-    return (
-        <section className='hint'>
-            <div className="header">AI 添削</div>
-            <p className="response">AI はまだ寝ている…</p>
-            <div className="input-area">
-                <p className="input" contentEditable></p>
-                <button className="send">
-                    <i className="fa-solid fa-paper-plane"></i>
-                </button>
-            </div>
-        </section>
-    );
+	return (
+		<section className='hint'>
+			<div className='header'>AI 添削</div>
+			<p className='response'>AI はまだ寝ている…</p>
+			<div className='input-area'>
+				<p className='input' contentEditable></p>
+				<button className='send'>
+					<i className='fa-solid fa-paper-plane'></i>
+				</button>
+			</div>
+		</section>
+	);
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,23 +8,25 @@ import Footer from 'components/Footer';
 import 'components/Main.css';
 
 export default function Main(props) {
-    const [currentTime, setCurrentTime] = useState(0);
-    const playerRef = useRef(null);
+	const [currentTime, setCurrentTime] = useState(0);
+	const playerRef = useRef(null);
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            if (playerRef.current && playerRef.current.getCurrentTime)
-            setCurrentTime(playerRef.current.getCurrentTime());
-        }, 100)
-        return () => clearInterval(interval);
-    }, []);
-    
-    return <>
-        <Nav/>
-        <main>
-            <Display playerRef={playerRef} currentTime={currentTime}/>
-            <Transcript playerRef={playerRef} currentTime={currentTime}/>
-        </main>
-        <Footer/>
-    </>
+	useEffect(() => {
+		const interval = setInterval(() => {
+			if (playerRef.current && playerRef.current.getCurrentTime)
+				setCurrentTime(playerRef.current.getCurrentTime());
+		}, 100);
+		return () => clearInterval(interval);
+	}, []);
+
+	return (
+		<>
+			<Nav />
+			<main>
+				<Display playerRef={playerRef} currentTime={currentTime} />
+				<Transcript playerRef={playerRef} currentTime={currentTime} />
+			</main>
+			<Footer />
+		</>
+	);
 }
