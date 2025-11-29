3f16c87bd7fdab164226874e20076998b2d6ad34
陳曦 <eriksonchen@gmail.com>
2025-11-29 23:29:46 +0800

    chore: change tab size for webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,88 +5,85 @@ const srcPath = path.resolve(__dirname, 'src');
 const distPath = path.resolve(__dirname, 'dist');
 
 module.exports = {
-  context: srcPath,
+    context: srcPath,
 
-  mode: 'development',
+    mode: 'development',
 
-  resolve: {
-    extensions: ['.js', '.jsx'], 
-    alias: {
-      components: path.resolve(srcPath, 'components'),
-      utilities: path.resolve(srcPath, 'utilities'),
-      data: path.resolve(srcPath, 'data'),
-    },
-  },
-
-  entry: {
-    index: './index.jsx',
-  },
-
-  output: {
-    path: distPath,
-    filename: '[name].bundle.js',
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              ['@babel/preset-env', { modules: false }],
-              '@babel/preset-react',
-            ],
-            plugins: [
-              '@babel/plugin-proposal-class-properties',
-              '@babel/plugin-proposal-object-rest-spread',
-            ],
-          },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+        alias: {
+            components: path.resolve(srcPath, 'components'),
+            utilities: path.resolve(srcPath, 'utilities'),
+            data: path.resolve(srcPath, 'data'),
         },
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              url: false,
+    },
+
+    entry: {
+        index: './index.jsx',
+    },
+
+    output: {
+        path: distPath,
+        filename: '[name].bundle.js',
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [['@babel/preset-env', { modules: false }], '@babel/preset-react'],
+                        plugins: [
+                            '@babel/plugin-proposal-class-properties',
+                            '@babel/plugin-proposal-object-rest-spread',
+                        ],
+                    },
+                },
             },
-          },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            url: false,
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                type: 'asset/resource',
+            },
         ],
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        type: 'asset/resource',
-      }
-    ],
-  },
+    },
 
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        vendor: {
-          minChunks: 2,
-          name: 'vendor',
-          chunks: 'all',
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    minChunks: 2,
+                    name: 'vendor',
+                    chunks: 'all',
+                },
+            },
         },
-      },
     },
-  },
 
-  devServer: {
-    static: {
-      directory: distPath,
-      watch: true,
+    devServer: {
+        static: {
+            directory: distPath,
+            watch: true,
+        },
+        compress: true,
+        port: 3000,
+        hot: true,
+        historyApiFallback: true,
     },
-    compress: true,
-    port: 3000,
-    hot: true,
-    historyApiFallback: true,
-  },
 
-  devtool: 'source-map',
+    devtool: 'source-map',
 };
